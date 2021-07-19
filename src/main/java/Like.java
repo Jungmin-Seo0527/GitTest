c7025ee5c0542322da28760fdaa4b76535f808be
Jungmin-Seo0527 <soato1405@naver.com>
2021-07-20 00:55:48 +0900

    싫어요 기능 추가

--- src/main/java/Like.java
+++ src/main/java/Like.java
@@ -2,5 +2,6 @@ public class Like {
     public static void main(String[] args) {
         System.out.println("좋아요 기능");
         System.out.println("싫어요 기능");
+        System.out.println("싫어요 기능");
     }
 }
